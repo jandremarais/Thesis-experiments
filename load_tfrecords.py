2a34aa964d76dc8c1628980eed3e2e20b09b6065
Jan <maraisjandre9@gmail.com>
2017-03-12 06:49:06 +0000

    first from RS server

--- load_tfrecords.py
+++ load_tfrecords.py
@@ -1,20 +1,84 @@
 import numpy as np
 import tensorflow as tf
 
-record = "/home/jan/Downloads/traina0.tfrecord"
+record = "/home/rstudio/traina0.tfrecord"
 
 vid_ids = []
 labels = []
 mean_rgb = []
 mean_audio = []
 
-for example in tf.python_io.tf_record_iterator(record):
+for example in tf.python_io.tf_record_iterator("/home/rstudio/traina0.tfrecord"):
     tf_example = tf.train.Example.FromString(example)
     vid_ids.append(tf_example.features.feature['video_id'].bytes_list.value[0].decode(encoding='UTF-8'))
     labels.append(tf_example.features.feature['labels'].int64_list.value)
     mean_rgb.append(tf_example.features.feature['mean_rgb'].float_list.value)
     mean_audio.append(tf_example.features.feature['mean_audio'].float_list.value)
 
+
+for example in tf.python_io.tf_record_iterator("/home/rstudio/traina1.tfrecord"):
+    tf_example = tf.train.Example.FromString(example)
+    vid_ids.append(tf_example.features.feature['video_id'].bytes_list.value[0].decode(encoding='UTF-8'))
+    labels.append(tf_example.features.feature['labels'].int64_list.value)
+    mean_rgb.append(tf_example.features.feature['mean_rgb'].float_list.value)
+    mean_audio.append(tf_example.features.feature['mean_audio'].float_list.value)
+    
+for example in tf.python_io.tf_record_iterator("/home/rstudio/traina2.tfrecord"):
+    tf_example = tf.train.Example.FromString(example)
+    vid_ids.append(tf_example.features.feature['video_id'].bytes_list.value[0].decode(encoding='UTF-8'))
+    labels.append(tf_example.features.feature['labels'].int64_list.value)
+    mean_rgb.append(tf_example.features.feature['mean_rgb'].float_list.value)
+    mean_audio.append(tf_example.features.feature['mean_audio'].float_list.value)
+    
+for example in tf.python_io.tf_record_iterator("/home/rstudio/traina3.tfrecord"):
+    tf_example = tf.train.Example.FromString(example)
+    vid_ids.append(tf_example.features.feature['video_id'].bytes_list.value[0].decode(encoding='UTF-8'))
+    labels.append(tf_example.features.feature['labels'].int64_list.value)
+    mean_rgb.append(tf_example.features.feature['mean_rgb'].float_list.value)
+    mean_audio.append(tf_example.features.feature['mean_audio'].float_list.value)
+    
+for example in tf.python_io.tf_record_iterator("/home/rstudio/traina4.tfrecord"):
+    tf_example = tf.train.Example.FromString(example)
+    vid_ids.append(tf_example.features.feature['video_id'].bytes_list.value[0].decode(encoding='UTF-8'))
+    labels.append(tf_example.features.feature['labels'].int64_list.value)
+    mean_rgb.append(tf_example.features.feature['mean_rgb'].float_list.value)
+    mean_audio.append(tf_example.features.feature['mean_audio'].float_list.value)
+    
+for example in tf.python_io.tf_record_iterator("/home/rstudio/traina5.tfrecord"):
+    tf_example = tf.train.Example.FromString(example)
+    vid_ids.append(tf_example.features.feature['video_id'].bytes_list.value[0].decode(encoding='UTF-8'))
+    labels.append(tf_example.features.feature['labels'].int64_list.value)
+    mean_rgb.append(tf_example.features.feature['mean_rgb'].float_list.value)
+    mean_audio.append(tf_example.features.feature['mean_audio'].float_list.value)
+    
+for example in tf.python_io.tf_record_iterator("/home/rstudio/traina6.tfrecord"):
+    tf_example = tf.train.Example.FromString(example)
+    vid_ids.append(tf_example.features.feature['video_id'].bytes_list.value[0].decode(encoding='UTF-8'))
+    labels.append(tf_example.features.feature['labels'].int64_list.value)
+    mean_rgb.append(tf_example.features.feature['mean_rgb'].float_list.value)
+    mean_audio.append(tf_example.features.feature['mean_audio'].float_list.value)
+    
+for example in tf.python_io.tf_record_iterator("/home/rstudio/traina7.tfrecord"):
+    tf_example = tf.train.Example.FromString(example)
+    vid_ids.append(tf_example.features.feature['video_id'].bytes_list.value[0].decode(encoding='UTF-8'))
+    labels.append(tf_example.features.feature['labels'].int64_list.value)
+    mean_rgb.append(tf_example.features.feature['mean_rgb'].float_list.value)
+    mean_audio.append(tf_example.features.feature['mean_audio'].float_list.value)
+    
+for example in tf.python_io.tf_record_iterator("/home/rstudio/traina8.tfrecord"):
+    tf_example = tf.train.Example.FromString(example)
+    vid_ids.append(tf_example.features.feature['video_id'].bytes_list.value[0].decode(encoding='UTF-8'))
+    labels.append(tf_example.features.feature['labels'].int64_list.value)
+    mean_rgb.append(tf_example.features.feature['mean_rgb'].float_list.value)
+    mean_audio.append(tf_example.features.feature['mean_audio'].float_list.value)
+    
+for example in tf.python_io.tf_record_iterator("/home/rstudio/traina9.tfrecord"):
+    tf_example = tf.train.Example.FromString(example)
+    vid_ids.append(tf_example.features.feature['video_id'].bytes_list.value[0].decode(encoding='UTF-8'))
+    labels.append(tf_example.features.feature['labels'].int64_list.value)
+    mean_rgb.append(tf_example.features.feature['mean_rgb'].float_list.value)
+    mean_audio.append(tf_example.features.feature['mean_audio'].float_list.value)
+    
 from numpy import array
 mean_audio = array(mean_audio)
 mean_rgb = array(mean_rgb)
