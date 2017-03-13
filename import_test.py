0b2310b502024712a9136d23bac2730db45060a9
Jan <maraisjandre9@gmail.com>
2017-03-13 19:28:32 +0000

    edits and adjustments for test import

--- import_test.py
+++ import_test.py
@@ -3,12 +3,9 @@ import tensorflow as tf
 import string
 import sys
 
-#filenames = map(str.strip, open("video_train_files.txt").readlines())
-#filenames = ["gs://youtube8m-ml/1/video_level/train/train-{}.tfrecord".format(i) for i in range(5)]
-filenames = map(str.strip, open(sys.argv[1]).readlines())
+filenames = map(str.strip, open("to_imp.txt").readlines())
 
 vid_ids = []
-labels = []
 mean_rgb = []
 mean_audio = []
 
@@ -16,7 +13,6 @@ for filename in filenames:
     for example in tf.python_io.tf_record_iterator(filename):
         tf_example = tf.train.Example.FromString(example)
         vid_ids.append(tf_example.features.feature['video_id'].bytes_list.value[0].decode(encoding='UTF-8'))
-        labels.append(tf_example.features.feature['labels'].int64_list.value)
         mean_rgb.append(tf_example.features.feature['mean_rgb'].float_list.value)
         mean_audio.append(tf_example.features.feature['mean_audio'].float_list.value)
 
@@ -25,7 +21,3 @@ mean_audio = array(mean_audio)
 mean_rgb = array(mean_rgb)
 
 X = np.hstack((mean_rgb, mean_audio))
-
-from sklearn import preprocessing
-lb = preprocessing.MultiLabelBinarizer()
-Y = lb.fit_transform(labels)
--- workflow.R
+++ workflow.R
@@ -9,21 +9,27 @@ system("gsutil ls gs://youtube8m-ml/1/video_level/test > video_test_files.txt")
 system("gsutil ls gs://youtube8m-ml/1/video_level/train > video_train_files.txt")
 system("gsutil ls gs://youtube8m-ml/1/video_level/validate > video_validate_files.txt")
 
-video_test_files <- read.table("video_test_files.txt", stringsAsFactors = FALSE)
-video_train_files <- read.table("video_train_files.txt", stringsAsFactors = FALSE)
-video_validate_files <- read.table("video_validate_files.txt", stringsAsFactors = FALSE)
+#video_test_files <- read.table("video_test_files.txt", stringsAsFactors = FALSE)
+#video_train_files <- read.table("video_train_files.txt", stringsAsFactors = FALSE)
+#video_validate_files <- read.table("video_validate_files.txt", stringsAsFactors = FALSE)
 
 main <- py_run_file("load_tfrecords.py")
 X <- main$X
 Y <- main$Y
 
-train_ind <- sample(1:nrow(X), 15000)
+train_ind <- sample(1:nrow(X), 2000)
 X_train <- X[train_ind, ]
 Y_train <- Y[train_ind, ]
 X_test <- X[-train_ind, ]
 Y_test <- Y[-train_ind, ]
 
-train_forest <- grow_forest(ntrees = 6, X_train, Y_train, max_leaf = 100)
+train_forest <- grow_forest(ntrees = 6, X, Y, max_leaf = 500)
+
+files <- read.table("video_test_files.txt", stringsAsFactors = FALSE)
+write.table(files[2, ], file = "to_imp.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+main <- py_run_file("import_test.py")
+X_test <- main$X
 
 Y_test_hat <- forest_predict(train_forest, X = X_test, Y = Y_test)
 
