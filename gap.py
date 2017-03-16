a4edbb6058433b8c6dc64c4d84b85032f523324f
Jan <maraisjandre9@gmail.com>
2017-03-16 14:54:30 +0000

    added feather compatability

--- gap.py
+++ gap.py
@@ -5,7 +5,11 @@ sys.path.append('youtube-8m')
 import eval_util
 
 import numpy as np
-labels_val = np.genfromtxt ('y.csv', delimiter=",")
-predictions_val = np.genfromtxt ('yhat.csv', delimiter=",")
+#labels_val = np.genfromtxt ('y.csv', delimiter=",")
+#predictions_val = np.genfromtxt ('yhat.csv', delimiter=",")
+
+import feather
+labels_val = feather.read_dataframe('y.feather').as_matrix()
+predictions_val = feather.read_dataframe('yhat.feather').as_matrix()
 
 value = eval_util.calculate_gap(predictions_val, labels_val)
--- workflow.R
+++ workflow.R
@@ -13,14 +13,7 @@ system("gsutil ls gs://youtube8m-ml/1/video_level/validate > video_validate_file
 
 # training step
 train_files <- read.table("video_train_files.txt", stringsAsFactors = FALSE)
-write.table(train_files[2:5, ], "train_to_imp.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
-
-main <- py_run_file("load_tfrecords.py")
-X_train <- main$X
-#Y_train <- main$Y
-
-y <- main$y
-y <- lapply(y, unlist)
+write.table(train_files[2:20, ], "train_to_imp.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 list2ind <- function(y, L = 4716) {
   Y <- matrix(0, ncol = L, nrow = length(y))
@@ -30,32 +23,46 @@ list2ind <- function(y, L = 4716) {
   Y
 }
 
+main <- py_run_file("load_tfrecords.py")
+X_train <- main$X
+#Y_train <- main$Y
+y <- main$y
+y <- lapply(y, unlist)
 Y_train <- list2ind(y = y)
 
-
-train_forest <- grow_forest(ntrees = 1, X_train, Y_train, max_leaf = 10)
-saveRDS(train_forest, "train_forest.rds")
+train_forest <- grow_forest(ntrees = 5, X_train, Y_train, max_leaf = 500)
+saveRDS(train_forest, "forest2-10.rds")
 train_forest <- readRDS("train_forest.rds")
 
-Y_hat_train_ <- forest_predict(train_forest, X = X_train)
+Y_hat_train <- forest_predict(train_forest[c(1,2,5)], X = X_train)
 
-write.table(Y_hat_train, "yhat.csv", sep = ",", row.names = FALSE, col.names = FALSE)
-write.table(Y_train, "y.csv", sep = ",", row.names = FALSE, col.names = FALSE)
+library(feather)
+write_feather(as.data.frame(Y_hat_train), 'yhat.feather')
+write_feather(as.data.frame(Y_train), 'y.feather')
+
+#write.table(Y_hat_train, "yhat.csv", sep = ",", row.names = FALSE, col.names = FALSE)
+#write.table(Y_train, "y.csv", sep = ",", row.names = FALSE, col.names = FALSE)
 
 py_run_file("gap.py")$value
 
 # validation step
 validate_files <- read.table("video_validate_files.txt", stringsAsFactors = FALSE)
-write.table(validate_files[2:5, ], "train_to_imp.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(validate_files[2:10, ], "train_to_imp.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 main <- py_run_file("load_tfrecords.py")
 X_validate <- main$X
-Y_validate <- main$Y
+#Y_validate <- main$Y
+y <- main$y
+y <- lapply(y, unlist)
+Y_validate <- list2ind(y = y)
 
-Y_hat_validate <- forest_predict(train_forest, X = X_validate)
+Y_hat_validate <- forest_predict(train_forest[c(1,2,5)], X = X_validate)
 
-write.table(Y_hat_validate, "yhat.csv", sep = ",", row.names = FALSE, col.names = FALSE)
-write.table(Y_validate, "y.csv", sep = ",", row.names = FALSE, col.names = FALSE)
+#write.table(Y_hat_validate, "yhat.csv", sep = ",", row.names = FALSE, col.names = FALSE)
+#write.table(Y_validate, "y.csv", sep = ",", row.names = FALSE, col.names = FALSE)
+
+write_feather(as.data.frame(Y_hat_validate), 'yhat.feather')
+write_feather(as.data.frame(Y_validate), 'y.feather')
 
 py_run_file("gap.py")$value
 
